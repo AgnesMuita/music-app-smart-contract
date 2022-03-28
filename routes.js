fed244ed5cc4a2e07e73e01a6255a4476b36b5d2
Agnes Muita <agnes.muita@alternateltd.com>
2022-03-28 13:03:53 +0300

    integrate the smart contract to the backend API using Web3

--- routes.js
+++ routes.js
@@ -1,8 +1,19 @@
 const shortid = require('short-id')
+const IPFS = require('ipfs-api');
+//initialize IPFS with the http client using the backend url as host and port 5001
+//This allows us to use IPFS methods to upload and retrieve data from IPFS
+const ipfs = IPFS({host:'ipfs.infura.io',port:5001,protocol:"https"})
+// import { nanoid } from 'nanoid';
+// nanoid = require('nanoid')
 
-function routes(app, db){
+function routes(app, dbe,lms, accounts){
+  let db = dbe.collection('music-users')
+  let music = dbe.collection('music-store')
+
   app.post('/register',(req,res)=>{
     let email = req.body.email
+    // model.id =nanoid();
+    // let idd = model.id
     let idd = shortid.generate()
     if(email){
       db.findOne({email},(err, doc)=>{
@@ -33,18 +44,45 @@ function routes(app, db){
         res.status(400).json({"status":"Failed","reason":"Wrong Input"})
     }
   })
-  app.post('/upload',(req,res)=>{
+  app.post('/upload',(req,res)=>async()=>{
       let buffer = req.body.buffer
       let name = req.body.name
       let title = req.body.title
+      let id = shortid.generate()+shortid.generate()
+
       if(buffer && title){
-      
+        //save the audio buffer to IPFS instead of storing it on the blockchain for anyone to use
+        let ibfsHash = await ibfs.add(buffer);
+        //save the buffer's address in the blockchain by generating an ID and using it as an identifier in the SendIPFS function
+        let hash = ibfsHash[0].hash
+        lms.sendIPFS(id,hash,{from:accounts[0]})
+        //save all the other data associated with the music file in our DB
+        .then((_hash,_address)=>{
+          music.insertOne({id, hash, title, name})
+          res.json({"status":"success",id})
+        })
+        .catch(err=>{
+            res.status(400).json({"status":"Failed","reason":"wrong input"})
+        })
       }else{
           res.status(400).json({"status":"Failed","reason":"Wrong input"})
       }
   })
   app.get('/access/:email/"id',(req,res)=>{
+    let id = req.params.id
     if(req.params.id && req.params.email){
+      db.findOne({email:req.body.email},(err,doc)=>{
+        if(doc){
+          //we retrieve our data by getting the id from the request using the id to access the IPFS hash address, and accessing the audio buffer using the address
+          lms.getHash(id, {from:accounts[0]})
+          .then(async(hash)=>{
+              let data = await ipfs.files.get(hash)
+              res.json({"status":"success",data:data.content})
+          })
+        }else{
+              res.status(400).json({"status":"Failed","reason":"Wrong input"})
+        }
+      })
     
     }else{
       res.status(400).json({"status":"Failed", "reason":"Wrong input"})
--- server.js
+++ server.js
@@ -8,9 +8,29 @@ const contract = require('truffle-contract');
 app.use(express.json())
 
 
-mongodb.connect(process.env.DB,{useUnifiedTopology:true},(err, client)=>{
-  const db = client.db('Cluster0')
-  routes(app, db)
+
+if(typeof Web3 !== "undefined")
+{
+  var web3 = new Web3(web3.currentProvider)
+}
+else{
+  var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost/8545'))
+}
+//build a contract based on the migrated json file and truffle contract package 
+const LMS = contract(artifacts);
+//set the contract provider to the web3 instance provider initialized on line 11
+LMS.setProvider(web3.currentProvider);
+
+mongodb.connect(process.env.DB,{useUnifiedTopology:true},async(err, client)=>{
+
+  const dbe = client.db('Cluster0')
+  //get accounts
+  const accounts = await web3.eth.getAccounts();
+  //lms is the instance of the contract, app is app instance, db is database instance, and the accounts data
+  const lms = await LMS.deployed();
+    //the commented function is called inputting the address as an argument if we have deployed our contract to a remote node 
+    //const lms = LMS.at(contract_address) for remote nodes deployed on ropsten or rinkeby
+  routes(app, dbe, lms, accounts)
   app.listen(process.env.PORT || 8082,()=>{
     console.log('listening on port 8082')
 
